b2235a3015afcfc8c12a5ae18b2047ab56535ff6
gary003 <gary.johnson003@gmail.com>
2024-09-08 15:55:52 +0700

    add more tests & better account gestion in function call

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# api_blockchain_school
-simple blockchain api school
+#  Blockchain api School
+
+## Description
+
+Basic blockchain API showing basic skills for a junior blockchain developer.
+
+## Skills description
+
+ - Solidity Development:
+Smart contract development on a blockchain network (Ethereum in this case)
+Contract creation and deployment using Truffle
+Error handling to prevent invalid operations
+
+- Testing:
+
+Unit testing with Mocha and Chai frameworks
+Test case creation for various scenarios
+Assertions to verify expected outcomes
+
+
+- Debugging: Identifying and resolving potential issues in the Solidity code
+Version control: Using Git to manage code changes
+Documentation: Writing clear and concise documentation like this README file
+
+## Prerequisites
+
+- Git for downloading the project.
+
+- Having npm installed (use of npx version > 10.7.0)
+OR
+- Having the truffle suite installed
+
+## Installation & Start
+
+- Clone the project :
+    `git clone https://github.com/gary003/api_blockchain_school.git`
+
+- Go into the project directory:
+    `cd api_blockchain_school`
+
+- Test the app:
+    `npx truffle test` 
+    OR
+    `truffle test` (if you have the truffle suite installed)
+
+## Developer
+
+ - Gary Johnson <gary.johnson.freelance@gmail.com>
+
+## License
+
+    [MIT]
--- test/School.test.js
+++ test/School.test.js
@@ -3,46 +3,140 @@ const School = artifacts.require('School')
 contract('School', accounts => {
     describe('School > addStudent', async () => {
         it('Should add a new student', async () => {
-            const contract = await School.deployed('School')
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[2]
 
-            const addResult = await contract.addStudent('0x8d9902647a7ab5b283245b2e024c60ec7e38dc28', 'Ela Uni', {from : accounts[0]})
+            const res = await contract.addStudent(studentAddress, 'Ela Uni', {from : accounts[0]}).catch(_ => null)
             
-            assert.equal(addResult.receipt.status, true, 'The update is successful (adding new student)')
+            assert.isNotNull(res)
+            assert.equal(res.receipt.status, true, 'The update is successful (adding new student)')
         })
         it('Should FAIL adding a new student (student already exists)', async () => {
-            const contract = await School.deployed('School')
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[2]
 
             try {
-                await contract.addStudent('0x8d9902647a7ab5b283245b2e024c60ec7e38dc28', 'Ela Uni', {from : accounts[0]})
+                await contract.addStudent(studentAddress, 'Ela Uni', {from : accounts[0]})
                 assert.fail('Unexpected success for adding new student')
             } catch(err) {
-                // console.log(await web3.eth.getAccounts(), accounts[0])
+                // console.log(await getAccounts(), accounts[0])
                 assert.equal(err.reason, 'Student already exists')
+            }
+        })
+        it('Should FAIL adding a new student (Only owner can do this)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[4]
+
+            try {
+                await contract.addStudent(studentAddress, 'Thomas Ilo', {from : accounts[1]})
+                assert.fail('Unexpected success for adding new student')
+            } catch(err) {
+                // console.log(await getAccounts(), accounts[0])
+                assert.equal(err.reason, 'You need to be the owner of the contract for that action')
             }
         })
     })
     describe('School > addGrade', async () => {
         it('Should successfully add a grade to a student', async () => {
-            const contract = await School.deployed('School')
+            const contract = await School.deployed('School', {from : accounts[0]})
             const bioField = School.Fields.Bio
+            const studentAddress = accounts[2]
 
-            const res = await contract.addGrade('0x8d9902647a7ab5b283245b2e024c60ec7e38dc28', 12, bioField)
+            const res = await contract.addGrade(studentAddress, 12, bioField, {from : accounts[0]}).catch(_ => null)
+            // console.log('Gas used:', res.receipt.gasUsed);
 
+            assert.isNotNull(res)
             assert.equal(res.receipt.status, true)
         })
-        it('Should fail adding a grade to a student (The student doesn\'t vexists', async () => {
-            const contract = await School.deployed('School')
+        it('Should fail adding a grade to a student (Only owner can do this)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+
+            const bioField = School.Fields.Bio
+
+            try {
+                await contract.addGrade(accounts[5], 12, bioField, {from : accounts[3]})
+                assert.fail('Error - Unexpected success while adding a grade')
+            } catch(err) {
+                assert.isNotNull(err)
+                assert.isNotNull(err.hijackedStack)
+                assert.isTrue(err.hijackedStack.includes('You need to be the owner of the contract for that action'))
+           }
+        })
+        it('Should fail adding a grade to a student (The student doesn\'t exists)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
 
             const bioField = School.Fields.Bio
             const fake_address = '0x8d9902647a7ab5b283245b2e024c60ec7e38dc22'
 
             try {
-                await contract.addGrade(fake_address, 12, bioField)
+                await contract.addGrade(fake_address, 12, bioField, {from : accounts[0]})
                 assert.fail('Error - Unexpected success while adding a grade')
             } catch(error) {
                 assert.equal(error.reason, 'Student doesn\'t exist')
             }
         })
+    })
+    describe('School > getStudentGrades', async () => {
+        it('Should successfully get a student grades', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[2]
 
+            const res = await contract.getStudentGrades(studentAddress, {from : accounts[0]}).catch(_ => null)
+
+            assert.isNotNull(res)
+            assert.isArray(res)
+            assert.isTrue(res.length > 0)
+        })
+        it('Should fail getting grades of a student (Only owner can do this)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+            
+            const studentAddress = accounts[2]
+
+            try {
+                await contract.getStudentGrades(studentAddress, {from : accounts[3]})
+                assert.fail('Error - Unexpected success while getting a grade')
+            } catch(err) {
+                assert.isNotNull(err)
+                assert.isNotNull(err.hijackedStack)
+                assert.isTrue(err.hijackedStack.includes('You need to be the owner of the contract for that action'))
+           }
+        })
+    })
+    describe('School > getStudentGrades', async () => {
+        it('Should successfully get a student grades(from contract owner)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[2]
+
+            const res = await contract.getStudentGrades(studentAddress, {from : accounts[0]}).catch(_ => null)
+
+            assert.isNotNull(res)
+            assert.isArray(res)
+            assert.isTrue(res.length > 0)
+
+        })
+        it('Should fail getting a student grades (user not the contract owner)', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+            const studentAddress = accounts[2]
+
+            try {
+                await contract.getStudentGrades(studentAddress, {from : accounts[2]})
+                assert.fail('Error - Unexpected success')
+            } catch(err) {
+                assert.isNotNull(err)
+                assert.isNotNull(err.hijackedStack)
+                assert.isTrue(err.hijackedStack.includes('You need to be the owner of the contract for that action'))
+            }
+        })
+    })
+    describe('School > getGrades', async () => {
+        it('Should successfully get a student own grades', async () => {
+            const contract = await School.deployed('School', {from : accounts[0]})
+
+            const res = await contract.getGrades({from : accounts[2]}).catch(_ => null)
+
+            assert.isNotNull(res)
+            assert.isArray(res)
+            assert.isTrue(res.length > 0)
+        })
     })
 })
